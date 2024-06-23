b9db2e8c9072ee192348e553e81ef56fe315aa7b
Proscka <proskinaviktoria861@gmail.com>
2024-06-23 12:28:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+name="Viktoria"
+print(name)
+age=45
+print(age)
+age=age+5
+print(age)
+age=15
+age=age+15
+age=17
+age=age+17
+is_student=("True")
+print(is_student)
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
